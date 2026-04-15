621a93a933fc5fb9e99a98277b30a5babb9fdb9b
Alice088 <alien280579@gmail.com>
2026-04-16 01:20:14 +0300

    feat(bash): add Windows process group support (CREATE_NEW_PROCESS_GROUP)

--- pkg/sys/set_sys_proc_attr_windows.go
+++ pkg/sys/set_sys_proc_attr_windows.go
@@ -4,7 +4,11 @@ package sys
 
 import (
 	"os/exec"
+	"syscall"
 )
 
 func SetSysProcAttr(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+	}
 }
